106df5ae06c239bcb6436c4d1589a55def53ed98
Joice Paz <JoicePS@tce.rj.gov.br>
2018-11-27 16:32:10 -0200

    Add project files.

---
+++ Miojo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Miojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
